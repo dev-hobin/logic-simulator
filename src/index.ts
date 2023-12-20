e4115ce9efaf5ff84105729083473ddc900e5003
장호빈 <devhobin@gmail.com>
2023-12-20 22:14:01 +0900

    feat: 탬플릿 스캐폴딩 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {
 import prompts, { type Answers } from 'prompts'
 import chalk from 'chalk'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs'
 
 type Framework = 'react' | 'vanilla'
 type Variant = 'js' | 'ts'
@@ -13,6 +15,9 @@ type Template = `${Framework}-${Variant}`
 
 const FRAMEWORKS: Framework[] = ['vanilla', 'react']
 const VARIANTS: Variant[] = ['js', 'ts']
+const RENAME_FILES: Record<string, string> = {
+  _gitignore: '.gitignore',
+}
 
 const isValidPath = (p: string) => !path.dirname(path.resolve(p)).includes('.')
 const validatePath = (p: string | undefined) => {
@@ -86,15 +91,75 @@ const init = async () => {
     return
   }
 
-  const projectDir = argProjectDir
-    ? path.resolve(argProjectDir)
-    : path.resolve(result.projectName)
-
   const [framework, variant] = argTemplate
     ? argTemplate.split('-')
     : [result.framework, result.variant]
 
-  console.log({ projectName: path.basename(projectDir), framework, variant })
+  const projectDir = argProjectDir
+    ? path.resolve(argProjectDir)
+    : path.resolve(result.projectName)
+
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    '../../templates',
+    `${framework}-${variant}`,
+  )
+
+  fs.mkdirSync(projectDir, { recursive: true })
+
+  for (const fileName of readDirectory(templateDir).filter(
+    (fileName) => fileName !== 'package.json',
+  )) {
+    const src = path.join(templateDir, fileName)
+    const dest = path.join(projectDir, RENAME_FILES[fileName] ?? fileName)
+
+    if (isDirectory(src)) {
+      copyDirectory(src, dest)
+    } else {
+      copyFile(src, dest)
+    }
+  }
+
+  const pkg = JSON.parse(
+    fs.readFileSync(path.join(templateDir, 'package.json'), 'utf-8'),
+  )
+  pkg.name = path.basename(projectDir)
+  writeFile(projectDir, 'package.json', JSON.stringify(pkg, null, 2))
+
+  console.log(`\n${chalk.bold('Done. Happy Coding!')}`)
+}
+
+const readDirectory = (path: string) => fs.readdirSync(path)
+const writeFile = (dirPath: string, name: string, content: string) => {
+  fs.writeFileSync(path.join(dirPath, name), content)
+}
+const copyFile = (srcPath: string, destPath: string) => {
+  fs.copyFileSync(srcPath, destPath)
+}
+const isDirectory = (path: string) => {
+  return fs.statSync(path).isDirectory()
+}
+const copyDirectory = (srcDirPath: string, destDirPath: string) => {
+  const stack = [{ src: srcDirPath, dest: destDirPath }]
+
+  while (stack.length > 0) {
+    const curr = stack.pop()
+    if (!curr) continue
+
+    const { src, dest } = curr
+    fs.mkdirSync(dest, { recursive: true })
+
+    for (const file of readDirectory(src)) {
+      const srcPath = path.resolve(src, file)
+      const destPath = path.resolve(dest, file)
+
+      if (isDirectory(srcPath)) {
+        stack.push({ src: srcPath, dest: destPath })
+      } else {
+        copyFile(srcPath, destPath)
+      }
+    }
+  }
 }
 
 init().catch(console.error)
