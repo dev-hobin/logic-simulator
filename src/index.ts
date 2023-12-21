f706d53ec3ce92cd30bac5f74a68d49af8a9d8b2
장호빈 <devhobin@gmail.com>
2023-12-21 17:24:03 +0900

    feat: overwrite 로직 구현

--- src/index.ts
+++ src/index.ts
@@ -44,74 +44,119 @@ program
   )
   .parse()
 
-const init = async () => {
-  const argProjectDir = program.processedArgs[0]
-  const argTemplate = program.opts<{ template?: Template }>().template
+const argProjectDir = program.processedArgs[0]
+const [argFramework, argVariant] = program
+  .opts<{ template?: Template }>()
+  .template?.split('-') ?? [undefined, undefined]
 
-  let result: Answers<'projectName' | 'framework' | 'variant'>
+const scaffold = async () => {
+  const initialValues = {
+    projectPath: argProjectDir ? path.resolve(argProjectDir) : undefined,
+    framework: argFramework,
+    variant: argVariant,
+    overwrite: 'no',
+  }
+  let answers: Answers<'projectPath' | 'framework' | 'variant' | 'overwrite'>
   try {
-    result = await prompts(
-      [
+    answers = {
+      ...initialValues,
+      ...(await prompts(
+        [
+          {
+            type: initialValues.projectPath ? null : 'text',
+            name: 'projectPath',
+            message: 'Project name:',
+            initial: 'logic-simulator',
+            format: (v) => path.resolve(v),
+            validate: (v) => (!isValidPath(v) ? 'invalid project name' : true),
+          },
+          {
+            type: (_, values) => {
+              const projectPath =
+                values.projectPath ?? initialValues.projectPath
+              return isDirectoryExist(projectPath) &&
+                !isEmptyDirectory(projectPath, { excludes: ['.git'] })
+                ? 'select'
+                : null
+            },
+            name: 'overwrite',
+            message:
+              'Target directory is not empty. Please choose how to proceed:',
+            initial: 0,
+            choices: [
+              {
+                title: 'Remove existing files and continue',
+                value: 'yes',
+              },
+              {
+                title: 'Cancel operation',
+                value: 'no',
+              },
+            ],
+          },
+          {
+            type: (_, values) => {
+              if (values?.overwrite === 'no') {
+                throw new Error(chalk.red('✖') + ' Operation cancelled')
+              }
+              return null
+            },
+            name: 'overwriteChecker',
+          },
+          {
+            type: initialValues.framework ? null : 'select',
+            name: 'framework',
+            message: 'Select a framework',
+            initial: 0,
+            choices: [
+              { title: chalk.yellow('Vanilla'), value: 'vanilla' },
+              { title: chalk.blue('React'), value: 'react' },
+            ],
+          },
+          {
+            type: initialValues.variant ? null : 'select',
+            name: 'variant',
+            message: 'Select a variant',
+            initial: 0,
+            choices: [
+              { title: chalk.yellow('Javascript'), value: 'js' },
+              { title: chalk.blue('Typescript'), value: 'ts' },
+            ],
+          },
+        ],
         {
-          type: argProjectDir ? null : 'text',
-          name: 'projectName',
-          message: 'Project name:',
-          initial: 'logic-simulator',
-          validate: (v) => (!isValidPath(v) ? 'invalid project name' : true),
+          onCancel: () => {
+            throw new Error(chalk.red('✖') + ' Operation cancelled')
+          },
         },
-        {
-          type: argTemplate ? null : 'select',
-          name: 'framework',
-          message: 'Select a framework',
-          initial: 0,
-          choices: [
-            { title: chalk.yellow('Vanilla'), value: 'vanilla' },
-            { title: chalk.blue('React'), value: 'react' },
-          ],
-        },
-        {
-          type: argTemplate ? null : 'select',
-          name: 'variant',
-          message: 'Select a variant',
-          initial: 0,
-          choices: [
-            { title: chalk.yellow('Javascript'), value: 'js' },
-            { title: chalk.blue('Typescript'), value: 'ts' },
-          ],
-        },
-      ],
-      {
-        onCancel: () => {
-          throw new Error(chalk.red('✖') + ' Operation cancelled')
-        },
-      },
-    )
+      )),
+    }
   } catch (cancelled: any) {
     console.log(cancelled.message)
     return
   }
 
-  const [framework, variant] = argTemplate
-    ? argTemplate.split('-')
-    : [result.framework, result.variant]
+  const { projectPath, framework, variant, overwrite } = answers
 
-  const projectDir = argProjectDir
-    ? path.resolve(argProjectDir)
-    : path.resolve(result.projectName)
-
-  const templateDir = path.resolve(
+  const templatePath = path.resolve(
     fileURLToPath(import.meta.url),
     '../../templates',
     `${framework}-${variant}`,
   )
 
-  fs.mkdirSync(projectDir, { recursive: true })
+  if (overwrite === 'yes') {
+    emptyDirectory(projectPath, { excludes: ['.git'] })
+  }
 
-  for (const fileName of readDirectory(templateDir).filter(
+  if (!isDirectoryExist(projectPath)) {
+    fs.mkdirSync(projectPath, { recursive: true })
+  }
+
+  for (const fileName of readDirectory(templatePath).filter(
     (fileName) => fileName !== 'package.json',
   )) {
-    const src = path.join(templateDir, fileName)
-    const dest = path.join(projectDir, RENAME_FILES[fileName] ?? fileName)
+    const src = path.join(templatePath, fileName)
+    const dest = path.join(projectPath, RENAME_FILES[fileName] ?? fileName)
 
     if (isDirectory(src)) {
       copyDirectory(src, dest)
@@ -121,12 +166,10 @@ const init = async () => {
   }
 
   const pkg = JSON.parse(
-    fs.readFileSync(path.join(templateDir, 'package.json'), 'utf-8'),
+    fs.readFileSync(path.join(templatePath, 'package.json'), 'utf-8'),
   )
-  pkg.name = path.basename(projectDir)
-  writeFile(projectDir, 'package.json', JSON.stringify(pkg, null, 2))
-
-  console.log(`\n${chalk.bold('Done. Happy Coding!')}`)
+  pkg.name = path.basename(projectPath)
+  writeFile(projectPath, 'package.json', JSON.stringify(pkg, null, 2))
 }
 
 const readDirectory = (path: string) => fs.readdirSync(path)
@@ -138,6 +181,33 @@ const copyFile = (srcPath: string, destPath: string) => {
 }
 const isDirectory = (path: string) => {
   return fs.statSync(path).isDirectory()
+}
+const isDirectoryExist = (path: string) => {
+  return fs.existsSync(path) && isDirectory(path)
+}
+const isEmptyDirectory = (
+  path: string,
+  { excludes }: { excludes: string[] } = { excludes: [] },
+) => {
+  const files = fs.readdirSync(path)
+  const excludesSet = new Set(excludes)
+
+  return (
+    files.length === 0 ||
+    (files.length === excludesSet.size &&
+      files.every((file) => excludesSet.has(file)))
+  )
+}
+const emptyDirectory = (
+  dir: string,
+  { excludes }: { excludes: string[] } = { excludes: [] },
+) => {
+  for (const file of readDirectory(dir)) {
+    if (excludes.includes(file)) {
+      continue
+    }
+    fs.rmSync(path.resolve(dir, file), { recursive: true, force: true })
+  }
 }
 const copyDirectory = (srcDirPath: string, destDirPath: string) => {
   const stack = [{ src: srcDirPath, dest: destDirPath }]
@@ -162,4 +232,6 @@ const copyDirectory = (srcDirPath: string, destDirPath: string) => {
   }
 }
 
-init().catch(console.error)
+scaffold()
+  .then(() => console.log(`\n${chalk.bold('Done. Happy Coding!')}`))
+  .catch(console.error)
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs'],
   target: 'node16',
+  minify: true,
   shims: true,
   clean: true,
 })
